ace5ff41033d6a2de690b26d27f1ec4633480fb3
Aditi Singh <787aditi@gmail.com>
2017-04-16 11:45:25 -0500

    Vacancy detection in parking lot

--- cardetection.cpp
+++ cardetection.cpp
@@ -14,10 +14,16 @@ using namespace cv;
 using namespace std;
 using namespace tinyxml2;
 
-int main()
+int main( int argc, char** argv )
 {
 
-	String car_cascade_name = "DS/lbp_1200/cascade.xml";
+	if( argc != 3)
+    {
+     cout <<" Usage: prog_exec cascade_file_path test_image_path" << endl;
+     return -1;
+    }
+
+	String car_cascade_name = argv[1];
 	CascadeClassifier car_cascade;
 	Mat frame, gray;
 	vector<Rect> cars;
@@ -36,39 +42,31 @@ int main()
 
 	if (!car_cascade.load(car_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
 
-	String file1="/home/aditi/Computer_Vision/HW2/PKLot-small/parking1a/cloudy/2012-12-12/2012-12-12_10_00_05.jpg";
-	String file2="/home/aditi/Computer_Vision/HW2/PKLot-small/parking1a/rainy/2012-12-07/2012-12-07_16_42_25.jpg";
-	String file3="/home/aditi/Computer_Vision/HW2/PKLot-small/parking1b/cloudy/2013-02-22/2013-02-22_17_10_11.jpg";
-	String file4="/home/aditi/Computer_Vision/HW2/PKLot-small/parking1b/rainy/2013-02-26/2013-02-26_13_04_33.jpg";
-	String file5="/home/aditi/Computer_Vision/HW2/PKLot-small/parking2/cloudy/2012-09-12/2012-09-12_07_23_35.jpg";
-	String file6="/home/aditi/Computer_Vision/HW2/PKLot-small/parking2/rainy/2012-09-16/2012-09-16_07_57_59.jpg";
-	
-	String file=file6;
+	String file=argv[2];
 	frame = imread(file);
 
 	cvtColor(frame, gray, CV_RGB2GRAY);
-	cout<<"Frame found"<<endl;
+	// cout<<"Frame found"<<endl;
 	car_cascade.detectMultiScale(gray, cars, 1.1, 10, 0, Size(40,40), Size(60,60));
-	cout<<"Detection done"<<endl;
-	cout <<"Cars found:"<<cars.size()<<endl;
+	// cout<<"Detection done"<<endl;
+	// cout <<"Cars found:"<<cars.size()<<endl;
 	for (int i = 0; i < cars.size(); i++)
 	{
-		rectangle(frame, Point(cars[i].x, cars[i].y), Point(cars[i].x + cars[i].width, cars[i].y + cars[i].height), Scalar(255, 0, 0), 1);
+		rectangle(frame, Point(cars[i].x, cars[i].y), Point(cars[i].x + cars[i].width, cars[i].y + cars[i].height), Scalar(255, 0, 0), 2);
 
 	}
-	cout<<"Rectangles drawn"<<endl;
-	// imwrite("detect_lbp_1200_1_1.jpg",frame);
-	cout<<"Image saved"<<endl;
+	// cout<<"Rectangles drawn"<<endl;
+	// cout<<"Image saved"<<endl;
 
 	//PARKING LOT ANALYSIS
 	std::string xml_file = file.operator std::string();
 	std::string str1="xml";
 
-	cout<<xml_file<<endl;
+	// cout<<xml_file<<endl;
 	unsigned length=xml_file.length();
 	std::string str_new=xml_file.substr(0,length-3);  
 	xml_file=str_new.append(str1);
-	cout<<xml_file<<endl;
+	// cout<<xml_file<<endl;
 	
 	char *cstr = new char[xml_file.length() + 1];
 	strcpy(cstr, xml_file.c_str());
@@ -159,7 +157,7 @@ int main()
 	for(vector<int>::iterator it=Occupied.begin();it!=Occupied.end();++it)
 		total_occupied+=*it;
 
-	cout<<"Occupied parking lots: "<<total_occupied<<endl;
+	// cout<<"Occupied parking lots: "<<total_occupied<<endl;
 
 	// float minimum_dist=2000;
 	float dist1, dist2, dist3, dist4, hypo;
@@ -169,6 +167,7 @@ int main()
 	//for every parking space, find the closest and possible car in it
 	for (int i = 0; i < SpaceNos; i++)
 	{	
+		cout<<endl<<"Space Number: "<<i<<endl;
 		occupied_flag=0;
 		if(Occupied[i]==1)
 		{
@@ -189,24 +188,38 @@ int main()
 			if(occupied_flag)
 				{
 					occupied_count=occupied_count+1;
-					circle(frame, Point(x_coord,y_coord), 3,Scalar(0,0,255), -1,8,0);
+					cout<<"Detected: Occupied"<<endl;
+					// circle(frame, Point(x_coord,y_coord), 3,Scalar(0,0,255), -1,8,0);
 				}
+				else{
+					cout<<"Detected: Vacant"<<endl;
+				}
+				if(Occupied[i])
+						cout<<"Actually: Occupied"<<endl;
+					else
+						cout<<"Actually: Vacant"<<endl;
+
+
 		}
 		Detected.push_back(occupied_flag);
 	}
 
-	for (int j = 0; j < cars.size(); j++)
-		circle(frame, Point(cars[j].x+0.5*cars[j].width,cars[j].y+0.5*cars[j].height), 3,Scalar(0,255,0), -1,8,0);
+	// for (int j = 0; j < cars.size(); j++)
+		// circle(frame, Point(cars[j].x+0.5*cars[j].width,cars[j].y+0.5*cars[j].height), 3,Scalar(0,255,0), -1,8,0);
 
 	
-	cout<<"Spaces :"<<SpaceNos<<" occupied total: "<<total_occupied<<", detected: "<<occupied_count<<" ,percentage: "<<100.0*occupied_count/total_occupied<<endl;
+	// cout<<endl<<"Spaces :"<<SpaceNos<<" occupied total: "<<total_occupied<<", detected: "<<occupied_count<<" ,percentage: "<<100.0*occupied_count/total_occupied<<endl;
 	// for (int i = 0; i < SpaceNos; i++)
 	// {
 	// 	cout<<Occupied[i]<<" "<<Detected[i]<<endl;
 	// }
+	cout<<endl<<"True Positives: "<<occupied_count<<" , False Negatives: "<<total_occupied-occupied_count<<", Accuracy: "<<100.0*occupied_count/total_occupied<<"%%"<<endl;
 
 	namedWindow("Frame", WINDOW_NORMAL);
     imshow("Frame",frame);
+    cout<<"Results saved as result.jpg"<<endl;
+    cout<<argv[2]<<endl;
+	imwrite("result.jpg",frame);
 	waitKey();
 	return 0;
 }
